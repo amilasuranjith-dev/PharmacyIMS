08a8e9274f0cb0c3f1e8ea77270117969ff94f2c
amilasuranjith-dev <amilasuranjith.dev@gmail.com>
2026-04-01 18:52:45 +0530

    refactor: remove unused FXMLLoader initialization in Starter class

--- src/main/java/edu/icet/Starter.java
+++ src/main/java/edu/icet/Starter.java
@@ -7,8 +7,7 @@ import javafx.stage.Stage;
 public class Starter extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/Dashboard.fxml"));
-        //need help
+
     }
     public static void main(String[] args) {
         launch(args);

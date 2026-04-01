4406dc88c2cb7d1a33f4a7ba4372f408773cfa8b
amilasuranjith-dev <amilasuranjith.dev@gmail.com>
2026-04-01 16:05:36 +0530

    init: update Main class to use Starter for application launch and add JavaFX dependencies

--- src/main/java/edu/icet/Starter.java
+++ src/main/java/edu/icet/Starter.java
@@ -1,4 +1,16 @@
 package edu.icet;
 
-public class Starter {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/Dashboard.fxml"));
+        //need help
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
